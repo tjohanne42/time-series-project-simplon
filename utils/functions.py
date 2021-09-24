e864de1b1a74503dbbd205a06e9d373a88050519
gwizdo51 <arthur.clement.51@gmail.com>
2021-09-24 12:40:15 +0200

    overhaul

--- utils/functions.py
+++ utils/functions.py
@@ -64,95 +64,96 @@ def generate_df_by_time_section(time_section="hour", save_path=None, verbose=Fal
         # drop duplicates
         df_result = df_original.drop_duplicates().reset_index(drop=True)
 
-        # set the timestamps minutes and seconds to 0
-        if verbose:
-            print("set seconds and minutes to 0 ...")
-            df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(second=0, minute=0))
-        else:
-            df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(second=0, minute=0))
-
-        # get the mean values or every hour
-        print("get the mean values for every hour ...") if verbose else ...
-        df_result = df_result.groupby(["timestamp"], as_index=False).mean()
-
-        # add null values to fill missing hours
-
-        # add a column "timestamp_day"
-        if verbose:
-            print("add a column timestamp_day")
-            df_result["timestamp_day"] = df_result["timestamp"].progress_map(lambda timestamp: datetime(timestamp.year, timestamp.month, timestamp.day))
-        else:
-            df_result["timestamp_day"] = df_result["timestamp"].map(lambda timestamp: datetime(timestamp.year, timestamp.month, timestamp.day))
-        # dictionary to store the missing hours
-        missing_hours = {"timestamp": []}
-        # place in "days" the list of all the unique recording days
-        days = df_result["timestamp_day"].unique()
-        if verbose:
-            print("look for missing recording hours ...")
-            days = tqdm(days)
-        # for each unique recording day ...
-        for day in days:
-            day = pd.to_datetime(day)
-            # place in df_tmp all recording hours of that day
-            df_tmp = df_result.loc[df_result["timestamp_day"] == day]
-            # if there is less than 24 hours in that day ...
-            if len(df_tmp) != 24:
-                # for every possible hours ...
-                for hour in range(24):
-                    # if that hour is missing ...
-                    if not np.any(df_tmp["timestamp"].dt.hour == hour):
-                        # add the missing hour to "missing hours"
-                        missing_hours["timestamp"].append(datetime(day.year, day.month, day.day, hour, 0, 0))
-        # create a DataFrame to hold the missing hours
-        df_missing_hours = pd.DataFrame.from_dict(missing_hours)
-        # add the missing hours to df_result, sort by timestamp and reset the index
-        print("add the missing hours to df_result, sort by timestamp and reset index") if verbose else ...
-        df_result = df_result.append(df_missing_hours).sort_values(by="timestamp").reset_index(drop=True)
-
-        # drop the column "timestamp_day"
-        print("drop the column timestamp_day") if verbose else ...
-        df_result = df_result.drop(columns="timestamp_day")
-
-        # interpolation on the energy columns to fill
-        # the null values of the missing hours
-        print("linear interpolation to fill the missing values ...") if verbose else ...
-        df_result.iloc[:, 1:] = df_result.iloc[:, 1:].interpolate(method="linear", axis="index")
-
-        # multiply by 12 to get the actual values by hour
-        # (the original values were for every 5 minutes)
-        print("multiply all values by 12 to get the actual values by hour ...") if verbose else ...
-        df_result.iloc[:, 1:] = df_result.iloc[:, 1:] * 12
-
         if time_section == "hour":
 
-            # return df_result as is
-            pass
+            # set the timestamps minutes and seconds to 0
+            if verbose:
+                print("set seconds and minutes to 0 ...")
+                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(second=0, minute=0))
+            else:
+                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(second=0, minute=0))
+
+            # get the meant values or every hour
+            print("get the mean values for every hour ...") if verbose else ...
+            df_result = df_result.groupby(["timestamp"], as_index=False).mean()
+
+            # add null values to fill missing hours
+
+            # add a column "timestamp_day"
+            if verbose:
+                print("add a column timestamp_day")
+                df_result["timestamp_day"] = df_result["timestamp"].progress_map(lambda timestamp: datetime(timestamp.year, timestamp.month, timestamp.day))
+            else:
+                df_result["timestamp_day"] = df_result["timestamp"].map(lambda timestamp: datetime(timestamp.year, timestamp.month, timestamp.day))
+            # dictionary to store the missing hours
+            missing_hours = {"timestamp": []}
+            # place in "days" the list of all the unique recording days
+            days = df_result["timestamp_day"].unique()
+            if verbose:
+                print("look for missing recording hours ...")
+                days = tqdm(days)
+            # for each unique recording day ...
+            for day in days:
+                day = pd.to_datetime(day)
+                # place in df_tmp all recording hours of that day
+                df_tmp = df_result.loc[df_result["timestamp_day"] == day]
+                # if there is less than 24 hours in that day ...
+                if len(df_tmp) != 24:
+                    # for every possible hours ...             ##### TOCHANGE
+                    for hour in range(24):
+                        # if that hour is missing ...
+                        if not np.any(df_tmp["timestamp"].dt.hour == hour):
+                            # add he missing hour to "missing hours"
+                            missing_hours["timestamp"].append(datetime(day.year, day.month, day.day, hour, 0, 0))
+            # create a DataFrame to hold the missing hours
+            df_missing_hours = pd.DataFrame.from_dict(missing_hours)
+            # add the missing hours to df_result, sort by timestamp and reset the index
+            print("add the missing hours to df_result, sort by timestamp and reset index") if verbose else ...
+            df_result = df_result.append(df_missing_hours).sort_values(by="timestamp").reset_index(drop=True)
+
+            # drop the column "timestamp_day"
+            print("drop the column timestamp_day") if verbose else ...
+            df_result = df_result.drop(columns="timestamp_day")
+
+            # interpolation on the energy columns to fill
+            # the null values of the missing hours
+            print("linear interpolation to fill the missing values ...") if verbose else ...
+            df_result.iloc[:, 1:] = df_result.iloc[:, 1:].interpolate(method="linear", axis="index")
+
+            # multiply by 12 to get the actual values by hour
+            # (the original values were for every 5 minutes)
+            print("multiply all values by 12 to get the actual values by hour ...") if verbose else ...
+            df_result.iloc[:, 1:] = df_result.iloc[:, 1:] * 12
 
         elif time_section == "day":
 
-            # set the timestamps hours to 0
+            # set the timestamps seconds, minutes and hours to 0
             if verbose:
-                print("set the timestamp hours to 0 ...")
-                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(hour=0))
+                print("set seconds, minutes and hours to 0 ...")
+                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(second=0, minute=0, hour=0))
             else:
-                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(hour=0))
+                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(second=0, minute=0, hour=0))
 
-            # get the sum of every hours for every days
-            print("get the sum of every hour for every day ...") if verbose else ...
-            df_result = df_result.groupby(["timestamp"], as_index=False).sum()
+            # get the meant values or every day
+            print("get the mean values for every day ...") if verbose else ...
+            df_result = df_result.groupby(["timestamp"], as_index=False).mean()
+
+            # multiply by 12 * 24 to get the actual values by day
+            print("multiply all values by 12 * 24 to get the actual values by day ...") if verbose else ...
+            df_result.iloc[:, 1:] = df_result.iloc[:, 1:] * 12 * 24
 
         elif time_section == "week":
 
-            # set the timestamps hours to 0
+            # set the timestamps seconds, minutes and hours to 0
             if verbose:
-                print("set the timestamp hours to 0 ...")
-                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(hour=0))
+                print("set the timestamp seconds, minutes and hours to 0 ...")
+                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(second=0, minute=0, hour=0))
             else:
-                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(hour=0))
+                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(second=0, minute=0, hour=0))
 
-            # get the sum of every hours for every days
-            print("get the sum of every hours for every days ...") if verbose else ...
-            df_result = df_result.groupby(["timestamp"], as_index=False).sum()
+            # get the mean of every hours for every days
+            print("get the mean of every hours for every days ...") if verbose else ...
+            df_result = df_result.groupby(["timestamp"], as_index=False).mean()
 
             # add the column "week"
             print("add a column week") if verbose else ...
@@ -175,9 +176,9 @@ def generate_df_by_time_section(time_section="hour", save_path=None, verbose=Fal
             else:
                 df_result["year"] = df_result.apply(associate_week_with_year, axis=1)
 
-            # get the sum of every days for every weeks
-            print("get the sum of every day for every week ...") if verbose else ...
-            df_tmp_1 = df_result.groupby(by=["year","week"], as_index=False).sum()
+            # get the mean of every days for every weeks
+            print("get the mean of every day for every week ...") if verbose else ...
+            df_tmp_1 = df_result.groupby(by=["year","week"], as_index=False).mean()
 
             # retrieve the timestamps for the mondays of every week
             print("retrive the timestamp of the first day of each week ...") if verbose else ...
@@ -189,31 +190,47 @@ def generate_df_by_time_section(time_section="hour", save_path=None, verbose=Fal
             # reorganise the columns
             df_result = df_tmp_1[["timestamp", "year", "week", "coal", "nuclear", "wind", "hydro", "solar"]]
 
+            # multiply by 12 * 24 * 7 to get the actual values by week
+            print("multiply all values by 12 * 24 * 7 to get the actual values by week ...") if verbose else ...
+            df_result.iloc[:, 1:] = df_result.iloc[:, 1:] * 12 * 24 * 7
+
         elif time_section == "month":
 
-            # set the timestamps hours to 0 and days to 1
+            # set the timestamps seconds, minutes and hours to 0 and days to 1
             if verbose:
-                print("set the timestamp hours to 0 and days to 1 ...")
-                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(hour=0, day=1))
+                print("set the timestamp seconds, minutes and hours to 0 and days to 1 ...")
+                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(second=0, minute=0, hour=0, day=1))
             else:
-                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(hour=0, day=1))
+                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(second=0, minute=0, hour=0, day=1))
 
-            # get the sum of every hours for every months
-            print("get the sum of every hour for every month ...") if verbose else ...
-            df_result = df_result.groupby(["timestamp"], as_index=False).sum()
+            # get the mean of every hours for every months
+            print("get the mean of every hour for every month ...") if verbose else ...
+            df_result = df_result.groupby(["timestamp"], as_index=False).mean()
+
+            # multiply by 12 * 24 to get the actual values by day
+            print("multiply all values by 12 * 24 * 7 to get the actual values by day ...") if verbose else ...
+            df_result.iloc[:, 1:] = df_result.iloc[:, 1:] * 12 * 24
+
+            print("/!\\ THE DATAFRAMES CONTAINS THE MEAN VALUES PER DAY IN THE MONTH /!\\")
 
         elif time_section == "year":
 
-            # set the timestamps hours to 0, days to 1 and months to 1
+            # set the timestamps seconds, minutes and hours to 0, days to 1 and months to 1
             if verbose:
-                print("set the timestamp hours to 0, days to 1 and months to 1 ...")
-                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(hour=0, day=1, month=1))
+                print("set the timestamp seconds, minutes and hours to 0, days to 1 and months to 1 ...")
+                df_result["timestamp"] = df_result["timestamp"].progress_map(lambda timestamp: timestamp.replace(second=0, minute=0, hour=0, day=1, month=1))
             else:
-                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(hour=0, day=1, month=1))
+                df_result["timestamp"] = df_result["timestamp"].map(lambda timestamp: timestamp.replace(second=0, minute=0, hour=0, day=1, month=1))
 
-            # get the sum of every hours for every years
-            print("get the sum of every hour for every year ...") if verbose else ...
-            df_result = df_result.groupby(["timestamp"], as_index=False).sum()
+            # get the mean of every hours for every months
+            print("get the mean of every hour for every YEAR ...") if verbose else ...
+            df_result = df_result.groupby(["timestamp"], as_index=False).mean()
+
+            # multiply by 12 * 24 to get the actual values by day
+            print("multiply all values by 12 * 24 * 7 to get the actual values by day ...") if verbose else ...
+            df_result.iloc[:, 1:] = df_result.iloc[:, 1:] * 12 * 24
+
+            print("/!\\ THE DATAFRAMES CONTAINS THE MEAN VALUES PER DAY IN THE YEAR /!\\")
 
     # save the csv to save_path if provided
     if save_path:
